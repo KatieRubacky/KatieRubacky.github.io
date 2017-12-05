4f1eb6059b965a90386aeb797b80210017acdde1
KatieRubacky <katierubacky@gmail.com>
2017-12-04 20:34:17 -0500

    Added videos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
       $('.parallax').parallax();
 });
+$('.carousel-control').click(function(e){
+            e.preventDefault();
+            $('#myCarousel').carousel( $(this).data() );
+});
+AP.resize();
